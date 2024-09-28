f7cd4c629c1bc144c3b5ae7c8d86855043068938
Rodrigo Alexander Sabino Ramirez <u20201b801@upc.edu.pe>
2024-09-28 01:37:42 -0500

    chore: initial project setup

---
+++ build/Desktop_x86_windows_msvc2022_pe_32bit-Debug/qtcsettings.cmake
@@ -0,0 +1,10 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("QT_QMAKE_EXECUTABLE" "C:/Qt/6.8.0/msvc2022_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Users/rodri/OneDrive/Documentos/Projects/RapidWebForgeApp/build/Desktop_x86_windows_msvc2022_pe_32bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "C:/Qt/6.8.0/msvc2022_64" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "C:/Program Files/LLVM/bin/clang-cl.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_COMPILER" "C:/Program Files/LLVM/bin/clang-cl.exe" CACHE "FILEPATH" "" FORCE)
