ed932f80702e6ef429b20dbe4a5205321ba1a274
lecav30 <lecav30@outlook.com>
2024-10-01 12:43:46 -0500

    fix: use llvm in windows

--- build/Desktop_x86_windows_msvc2022_pe_32bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_RapidWebForgeApp_FILE C:/Users/rodri/OneDrive/Documentos/Projects/RapidWebForgeApp/build/Desktop_x86_windows_msvc2022_pe_32bit-Debug/RapidWebForgeApp.exe)
-set(__QT_DEPLOY_TARGET_RapidWebForgeApp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_RapidWebForgeApp_RUNTIME_DLLS C:/Qt/6.8.0/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.8.0/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.8.0/msvc2022_64/bin/Qt6Cored.dll)
