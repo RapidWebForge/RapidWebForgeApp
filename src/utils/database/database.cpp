1a4e4bfda18369fb9148c3fbc97d47a16933a088
lecav30 <lecav30@outlook.com>
2024-10-02 17:08:15 -0500

    feat(database): add singleton

---
+++ src/utils/database/database.cpp
@@ -0,0 +1,35 @@
+#include "database.h"
+#include <fmt/core.h>
+#include <iostream>
+
+std::mutex Database::dbMutex;
+
+Database &Database::getInstance(const std::string &dbName)
+{
+    static Database instance(dbName);
+    return instance;
+}
+
+sqlite3 *Database::getConnection()
+{
+    return db;
+}
+
+Database::Database(const std::string &dbName)
+{
+    // Intentar abrir la base de datos en la ruta especificada
+    if (sqlite3_open(dbName.c_str(), &db)) {
+        fmt::print(stderr, "Error al abrir la base de datos '{}': {}\n", dbName, sqlite3_errmsg(db));
+        db = nullptr;
+    } else {
+        fmt::print("Base de datos abierta exitosamente en: {} \n", dbName);
+    }
+}
+
+Database::~Database()
+{
+    if (db) {
+        sqlite3_close(db);
+        fmt::print("Conexión a la base de datos cerrada.\n");
+    }
+}
---
+++ src/utils/database/database.h
@@ -0,0 +1,25 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <mutex>
+#include <sqlite3.h>
+#include <string>
+
+class Database
+{
+public:
+    static Database &getInstance(const std::string &dbName = "../db/projects.db");
+    sqlite3 *getConnection();
+    ~Database();
+
+    // Prohibir copia y asignación
+    Database(const Database &) = delete;
+    Database &operator=(const Database &) = delete;
+
+private:
+    Database(const std::string &dbName);
+    sqlite3 *db;
+    static std::mutex dbMutex;
+};
+
+#endif // DATABASE_H
