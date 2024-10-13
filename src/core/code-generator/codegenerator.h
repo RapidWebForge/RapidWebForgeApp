47f60923ebc2ebbe89f6fec3f86e9bcd5a80543f
lecav30 <lecav30@outlook.com>
2024-10-12 22:50:57 -0500

    feat(generate): create frontend folder and json

--- src/core/code-generator/codegenerator.h
+++ src/core/code-generator/codegenerator.h
@@ -3,18 +3,22 @@
 
 #include "../../models/project/project.h"
 #include "../backend-generator/backendgenerator.h"
+#include <nlohmann/json.hpp>
 
 class CodeGenerator
 {
 public:
-    explicit CodeGenerator(const Project &projectPath);
+    explicit CodeGenerator(const Project &project);
     BackendGenerator backendGenerator;
 
-public:
-    bool createBaseBackendProject();
-    bool createBaseFrontendProject();
+    bool createBaseBackendProject();  // Create backend project
+    bool createBaseFrontendProject(); // Create frontend project
 
 private:
+    bool createDirectory(const std::string &path);
+    bool unzipFile(const std::string &zipPath, const std::string &extractPath);
+    bool createJsonFile(const std::string &filePath, const nlohmann::json &jsonData);
+
     Project project;
 };
 
