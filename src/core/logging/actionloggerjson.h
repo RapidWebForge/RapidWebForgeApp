58412cf0619085d873bfdf0b643e1a5ac3076a4b
Rodrigo Alexander Sabino Ramirez <u20201b801@upc.edu.pe>
2025-01-25 22:07:01 -0500

    feat(logging): add JSON-based logger for user actions

---
+++ src/core/logging/actionloggerjson.h
@@ -0,0 +1,19 @@
+#ifndef ACTIONLOGGERJSON_H
+#define ACTIONLOGGERJSON_H
+
+#include <nlohmann/json.hpp> // Incluye la biblioteca JSON
+#include <string>
+
+class ActionLoggerJson
+{
+public:
+    explicit ActionLoggerJson(const std::string &logFilePath);
+    void logAction(const std::string &action, const std::string &componentID);
+
+private:
+    std::string logFilePath;
+    nlohmann::json readLogFile();                     // Leer archivo JSON existente
+    void writeLogFile(const nlohmann::json &logData); // Escribir archivo JSON
+    void ensureLogFileExists();
+};
+#endif // ACTIONLOGGERJSON_H
